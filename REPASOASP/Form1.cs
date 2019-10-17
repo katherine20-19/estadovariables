c773fd22302a533cb7fcda119caa406d06245a21
Usuario <kbarreiro9@misena.edu.co>
2019-10-17 16:21:03 -0500

    Variables

---
+++ REPASOASP/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace REPASOASP
+{
+    
+    public partial class Form1 : Form
+    {
+        private int uno;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textNunero.Text = "1";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            uno++;
+            textNunero.Text = uno.ToString();
+
+
+        }
+    }
+}
